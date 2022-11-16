febd2217e16b164e57ab618b954f6ebc844f11d6
rtaylor02 <taylor.rod.j@gmail.com>
2022-11-16 12:04:47 +0000

    Added JPA and H2 dependency

--- backend/cardatabase/src/main/java/com/rtaylor02/cardatabase/CardatabaseApplication.java
+++ backend/cardatabase/src/main/java/com/rtaylor02/cardatabase/CardatabaseApplication.java
@@ -1,14 +1,21 @@
 package com.rtaylor02.cardatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class CardatabaseApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(CardatabaseApplication.class, args);
+		logger.info("\t==============================");
+		logger.info("\tApplication started...");
+		logger.info("\t==============================");
+
 	}
 
 }
